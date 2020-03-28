31de6387d4586a4d8147642ba610a518c5a892e0
jenny5546 <jenny5546@naver.com>
2020-03-28 18:02:30 +0900

    selection sort done

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -3,6 +3,23 @@
 선택정렬을 파이썬으로 짜서 연습해보자!
 """
 
-array = [5,3,4,2,1]
+array = [0,5,31,3,4,2,1]
 
 def sort(arr, n):
+    
+    for i in range(n-1,-1,-1): # n-1부터 0 까지 downcount 
+        max = arr[i]
+        max_pos = i
+        for j in range(i): # 0 부터 i-1까지
+            if max < arr[j]:
+                max = arr[j]
+                max_pos = j
+        arr[max_pos],arr[i]= arr[i],arr[max_pos]
+            
+    print(arr)
+
+sort(array,len(array))
+
+                
+        
+
