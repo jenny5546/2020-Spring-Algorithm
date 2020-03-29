398658bf47900aba76fa0ed877787bceb03f6002
jenny5546 <jenny5546@naver.com>
2020-03-29 14:10:06 +0900

    bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -2,3 +2,14 @@
 문병로 교수님 알고리즘 4강. 정렬
 버블정렬을 파이썬으로 짜서 연습해보자!
 """
+
+array = [0,5,31,3,4,2,1]
+
+def sort(arr, n):
+    for end in range(n-1,-1,-1): # n-1부터 0 까지 downcount 
+        for i in range(end):
+            if arr[i]>arr[i+1]:
+                arr[i], arr[i+1]= arr[i+1], arr[i]
+    print(arr)
+
+sort(array,len(array))
