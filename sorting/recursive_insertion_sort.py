534efb67280f238ec186e72fae69d28c2d5d7aaf
jenny5546 <jenny5546@naver.com>
2020-03-29 14:56:06 +0900

    added coments

--- sorting/recursive_insertion_sort.py
+++ sorting/recursive_insertion_sort.py
@@ -26,7 +26,7 @@ def sort2(arr,n):
     if n==1:
         return -1
     else:
-        i = len(arr)-(n-1)
+        i = len(arr)-(n-1) # index를 하나씩 늘여가면서 할 것이기에, 시작 1 , 그다음 2...
         while i!=0 and arr[i]<arr[i-1] :
             arr[i],arr[i-1]=arr[i-1],arr[i]
             i=i-1
