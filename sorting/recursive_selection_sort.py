42246a43934c14112ce9d94b0b7f6413bbc05a06
jenny5546 <jenny5546@naver.com>
2020-03-29 11:54:03 +0900

    recursive selection

--- sorting/recursive_selection_sort.py
+++ sorting/recursive_selection_sort.py
@@ -2,3 +2,25 @@
 문병로 교수님 알고리즘 4강. 정렬
 선택정렬을 파이썬으로 짜서 연습해보자!(recursive.ver)
 """
+
+array = [0,5,31,3,4,2,1]
+
+
+def sort(arr,n):
+    # 기저사례
+    if n==1:
+        return -1
+    else:
+        max = arr[n-1]
+        max_pos = n-1
+
+        for i in range(n-1): # n-1부터 0 까지 downcount 
+            if max<arr[i]:
+                max_pos = i
+                max = arr[i]  
+
+        arr[max_pos],arr[n-1]= arr[n-1],arr[max_pos]
+        sort(arr,n-1)
+        
+sort(array,len(array))
+print(array)
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -6,7 +6,7 @@
 array = [0,5,31,3,4,2,1]
 
 def sort(arr, n):
-    
+
     for i in range(n-1,-1,-1): # n-1부터 0 까지 downcount 
         max = arr[i]
         max_pos = i
@@ -14,8 +14,8 @@ def sort(arr, n):
             if max < arr[j]:
                 max = arr[j]
                 max_pos = j
-        arr[max_pos],arr[i]= arr[i],arr[max_pos]
-            
+        arr[max_pos],arr[i]= arr[i],arr[max_pos]  
+           
     print(arr)
 
 sort(array,len(array))
