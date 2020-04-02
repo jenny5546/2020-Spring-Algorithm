3a374ea7682880cb4b8ed56c1d94d448391688cd
jenny5546 <jenny5546@naver.com>
2020-04-02 15:40:36 +0900

    quick sort done

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -4,20 +4,25 @@
 """
 array = [0,5,31,3,4,2,1]
 
+def swap(a,b):
+    temp = a
+    a = b
+    b= temp
+
 def partition(arr,l,r):
     pivot = arr[l]
-    less = l
-    for elem in range(l+1,r):
+    last = l
+    for elem in range(l+1,r+1):
         if arr[elem]<pivot:
-            less+=1
-            temp =arr[less]
-            arr[less]=arr[elem]
+            last+=1
+            temp =arr[last]
+            arr[last]=arr[elem]
             arr[elem]=temp
 
-    temp2 =arr[less]
-    arr[less]=arr[l]
+    temp2 =arr[last]
+    arr[last]=arr[l]
     arr[l]=temp2
-    return less
+    return last
 
 def quickSort(arr, l,r):
     if l<r:
@@ -25,5 +30,5 @@ def quickSort(arr, l,r):
         quickSort(arr,l,pivot-1)
         quickSort(arr,pivot+1,r)
 
-quickSort(array,0,len(array))
+quickSort(array,0,len(array)-1)
 print(array)
