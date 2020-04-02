f1e9f4bff346a004428b25d1307b25ee420fa38f
jenny5546 <jenny5546@naver.com>
2020-04-02 15:03:55 +0900

    merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -2,52 +2,72 @@
 문병로 교수님 알고리즘 4강. 정렬
 병합 정렬을 파이썬으로 짜서 연습해보자!
 """
-array = [0,5,31,3,4,2,1]
-
-def mergeSort(array,lhs,rhs):
-
-    if len(array)==1:
-        return array
-    
-    mid = len(array)//2
-    print(mid)
-    mergeSort(array[:mid],lhs,mid)
-    mergeSort(array[mid:],mid+1,rhs)
-    return merge(array,lhs,mid,rhs)
-   
-    
-
-def merge(array,lhs,mid,rhs):
-
-    """
-
-    정렬되어 있는 두 배열 A[lhs...mid]와 A[mid+1...rhs]를 합하여 
-    정렬된 하나의 배열 A[lhs..rhs] 만들기
-
-    """
-    front = lhs
-    back = mid+1
-    result = [-1]*(len(array)-1)
-    elem = 0
-
-    while elem<len(array):
-        if front == mid+1: #앞 리스트 소진
-            result[elem]=array[back]
-            elem+=1
-            back+=1
-        elif back > rhs: #뒷 리스트 소진
-            result[elem]=array[front]
-            elem+=1
-            front+=1
-        elif array[front]<array[back]:
-            result[elem]=array[front]
-            elem+=1
-            front+=1
-        else:
-            result[elem]=array[back]
-            elem+=1
-            back+=1
-          
-    return result
-
-mergeSort(array,0,len(array)-1)
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    # Copy data to temp arrays L[] and R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # Merge the temp arrays back into arr[l..r] 
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted 
+def mergeSort(arr,l,r): 
+    if l < r: 
+  
+        # Same as (l+r)//2, but avoids overflow for 
+        # large l and h 
+        m = (l+(r-1))//2
+  
+        # Sort first and second halves 
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r) 
+  
+  
+# Driver code to test above 
+arr = [12, 11, 13, 5, 6, 7] 
+n = len(arr) 
+print ("Given array is") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+  
+mergeSort(arr,0,n-1) 
+print ("\n\nSorted array is") 
+for i in range(n): 
+    print ("%d" %arr[i]),
